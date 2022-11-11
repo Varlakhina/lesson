bced4ef9bcc086d2b7f5da4c5939348f78902d4a
Yulia <y.varlakhina@gmail.com>
2022-11-11 21:40:29 +0300

    add tasks 01 and 03

---
+++ task01/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int square = number2 * number2;
+
+if (number1 == square) Console.WriteLine($"число {number2} является квадратом числа {number1}");
+else
+{
+    Console.WriteLine($"число {number2} не является квадратом числа {number1}");
+}
---
+++ task03/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("введите число от 1 до 7");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 1) Console.WriteLine("понедельник");
+else if (number == 2) Console.WriteLine("вторник");
+else if (number == 3) Console.WriteLine("среда");
+else if (number == 4) Console.WriteLine("четверг");
+else if (number == 5) Console.WriteLine("пятница");
+else if (number == 6) Console.WriteLine("суббота");
+else if (number == 7) Console.WriteLine("восресенье");
+else
+{
+Console.WriteLine("введено не верное число");
+}
